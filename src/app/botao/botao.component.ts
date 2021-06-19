eef5e92ab33069da4dcb00c5d6427e0129b68992
fabioconejo <fabio.conejo@hotmail.com>
2021-06-19 05:46:51 -0300

    5 files modified

--- src/app/botao/botao.component.ts
+++ src/app/botao/botao.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./botao.component.css']
 })
 export class BotaoComponent implements OnInit {
+  corFundo: string;
+  corFonte: string;
+  sombra: string;
+  sombraActive: string;
+  @Input() cor: string;
+
   constructor() {}
 
-  ngOnInit() {}
-
-  @Input() corFundo: String;
-  @Input() corFonte: String;
+  ngOnInit() {
+    switch (this.cor) {
+      case 'azul':
+        this.corFonte = 'var(--white)';
+        this.corFundo = 'var(--blue-jeans)';
+        this.sombra = '0px 6px var(--saphire-blue)';
+        this.sombraActive = '0px 2px var(--saphire-blue)';
+        break;
+      default:
+        this.corFonte = '#000000';
+        this.corFundo = '#ffffff';
+        this.sombra = '#999999';
+        break;
+    }
+  }
 }
