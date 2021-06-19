69c8c1fcd3682c201c4b22cbf3956299947ca191
fabioconejo <fabio.conejo@hotmail.com>
2021-06-19 01:54:02 -0300

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { BotaoComponent } from './botao/botao.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, InicioComponent ],
+  declarations: [ AppComponent, HelloComponent, InicioComponent, BotaoComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
