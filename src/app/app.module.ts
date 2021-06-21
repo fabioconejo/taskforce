8c00eceb6154cc9c9926e6c9df82e05fb100dd1b
fabioconejo <fabio.conejo@hotmail.com>
2021-06-21 04:22:36 -0300

    2 files modified, 3 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { BotaoComponent } from './botao/botao.component';
+import { TarefaComponent } from './tarefa/tarefa.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, InicioComponent, BotaoComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    BotaoComponent,
+    TarefaComponent
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/tarefa/tarefa.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tarefa',
+  templateUrl: './tarefa.component.html',
+  styleUrls: ['./tarefa.component.css']
+})
+export class TarefaComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
