973d1c2eb2211cf3757d0db42941396445707839
fabioconejo <fabio.conejo@hotmail.com>
2021-06-21 21:44:31 -0300

    3 files modified

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -6,17 +6,11 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./painel.component.css']
 })
 export class PainelComponent implements OnInit {
-  nomeProfissao: string;
-  imgProfissao: string;
-  numTarefas: number;
   maxNumTarefas: number = 8;
 
   @Input() tarefas: any;
-  @Input() profissao: any;
 
   constructor() {}
 
-  ngOnInit() {
-    this.numTarefas = this.tarefas.length;
-  }
+  ngOnInit() {}
 }
