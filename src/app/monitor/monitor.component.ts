b812b992f752ca61d996abddcc343227d8a6a65a
fabioconejo <fabio.conejo@hotmail.com>
2021-06-22 17:35:41 -0300

    1 file modified

--- src/app/monitor/monitor.component.ts
+++ src/app/monitor/monitor.component.ts
@@ -7,8 +7,10 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class MonitorComponent implements OnInit {
   texto: string;
+  complemento: string;
   tempo: number = 10;
   tarefa: any;
+  cloneTarefas: any;
 
   @Input() tarefas: any;
   @Input() registro: any;
@@ -16,7 +18,7 @@ export class MonitorComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    this.tarefa = JSON.parse(JSON.stringify(this.tarefas));
+    this.cloneTarefas = JSON.parse(JSON.stringify(this.tarefas));
     let intervalo = setInterval(() => {
       this.mudarTarefa();
     }, this.tempo * 1000);
@@ -24,18 +26,47 @@ export class MonitorComponent implements OnInit {
 
   mudarTarefa() {
     var registro: any;
+    var j: number = 0;
+
+    //desabilitar tarefa anterior no registro
     for (var i = 0; i < this.registro.length; i++) {
-      if (this.registro[i].ativo && this.registro[i].id === this.tarefa[0].id) {
+      if (this.registro[i].ativo && this.registro[i].id === this.tarefa.id) {
         this.registro[i].ativo = false;
       }
     }
-    this.texto = this.tarefa
-      .sort(() => 0.5 - Math.random())
-      .slice(0, 1)[0].texto;
+
+    //sortear tarefa nova
+    this.cloneTarefas.sort(() => 0.5 - Math.random());
+    do {
+      j = Math.floor(Math.random() * (this.cloneTarefas.length - 1));
+    } while (this.cloneTarefas[j].id === this.tarefa.id);
+
+    this.tarefa = this.cloneTarefas[j];
+    this.texto = this.tarefa.texto;
+
+    switch (this.tarefa.tipo) {
+      case 'acao':
+        this.complemento = '';
+        this.texto = this.tarefa.texto;
+        break;
+      case 'estado':
+        j = Math.floor(Math.random() * (this.tarefa.eVerbo.length - 1));
+        this.complemento = this.tarefa.eVerbo[j];
+        this.texto = this.tarefa.eTexto[j];
+        break;
+      case 'escolha':
+        j = Math.floor(Math.random() * (this.tarefa.lista.length - 1));
+        this.complemento = this.tarefa.lista[j].texto;
+        this.texto = this.tarefa.texto + ' ' + this.complemento;
+        break;
+    }
+    this.complemento = this.tarefa.verbo;
+
+    //adicionar tarefa no registro
     registro = {
-      id: this.tarefa[0].id,
+      id: this.tarefa.id,
       ativo: true,
-      valor: this.tarefa[0].verbo
+      valor: this.tarefa.verbo
     };
     this.registro.push(registro);
   }
