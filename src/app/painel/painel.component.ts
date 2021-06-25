b171c322596b0b69d4e10d31f5b5302cd5850c28
fabioconejo <fabio.conejo@hotmail.com>
2021-06-24 21:55:53 -0300

    1 file modified

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -129,13 +129,23 @@ export class PainelComponent implements OnInit {
     var j: number;
     var verbo: string;
 
+    if (typeof this.tarefaSorteada === 'undefined') {
+      do {
+        j = Math.floor(Math.random() * this.amostraTarefa.length);
+        this.tarefaSorteada = this.amostraTarefa[j];
+      } while (this.amostraTarefa[j].tipo !== 'acao');
+    } else {
+      do {
+        j = Math.floor(Math.random() * this.amostraTarefa.length);
+        this.tarefaSorteada = this.amostraTarefa[j];
+      } while (this.amostraTarefa[j].id === this.tarefaSorteada.id);
+    }
+
     do {
       j = Math.floor(Math.random() * this.amostraTarefa.length);
       if (typeof this.tarefaSorteada === 'undefined') {
-        do {
-          j = Math.floor(Math.random() * this.amostraTarefa.length);
-          this.tarefaSorteada = this.amostraTarefa[j];
-        } while (this.amostraTarefa[j].tipo !== 'acao');
+        this.tarefaSorteada = this.amostraTarefa[j];
+        break;
       }
     } while (this.amostraTarefa[j].id === this.tarefaSorteada.id);
 
