e465c0ab2fefc819e839fc740fb0289c792ccd1c
fabioconejo <fabio.conejo@hotmail.com>
2021-06-26 22:20:20 -0300

    2 files added

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,13 @@
+export const environments = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAGJgSHsEC72mz4mR9KKgFRdO16T9bS9bY',
+    authDomain: 'taskforce-384fa.firebaseapp.com',
+    databaseURL: 'https://taskforce-384fa-default-rtdb.firebaseio.com',
+    projectId: 'taskforce-384fa',
+    storageBucket: 'taskforce-384fa.appspot.com',
+    messagingSenderId: '626310038273',
+    appId: '1:626310038273:web:ef5825e7f58f740a5d9100',
+    measurementId: 'G-CPWCCRF4JQ'
+  }
+};
