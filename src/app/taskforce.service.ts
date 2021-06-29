124f1c752a7ed14f096266445da1e85032447f83
fabioconejo <fabio.conejo@hotmail.com>
2021-06-28 23:26:31 -0300

    2 files modified

--- src/app/taskforce.service.ts
+++ src/app/taskforce.service.ts
@@ -9,68 +9,14 @@ import firebase from 'firebase/app';
 export class TaskforceService {
   constructor(private db: AngularFireDatabase, private http: HttpClient) {}
   private ngUnsubscribe = new Subject();
-  private timestamp: number;
 
   unsubscribe() {
     this.ngUnsubscribe.next();
     this.ngUnsubscribe.complete();
   }
 
-  updateOnDisconnect(keySala: string, keyJogador: string) {
-    this.db
-      .object('salas/' + keySala + '/jogadores/' + keyJogador)
-      .query.ref.onDisconnect()
-      .remove();
-  }
-
-  getTimestamp(): number {
-    this.db
-      .object('timestamp')
-      .update({ timestamp: firebase.database.ServerValue.TIMESTAMP });
-
-    return this.timestamp;
-  }
-
-  monitorarTimestamp() {
-    this.db
-      .object('timestamp')
-      .valueChanges()
-      .pipe(takeUntil(this.ngUnsubscribe))
-      .subscribe((t: number) => {
-        this.timestamp = t;
-      });
-  }
-
   getProfissoes(): any {
     return this.http.get('assets/json/taskforce.json');
-  }
-
-  monitorarJogadores(keySala: string) {
-    var intervaloOffline: number = 20000;
-    var intervaloKick: number = 60000;
-
-    this.db
-      .object('salas/' + keySala + '/jogadores/')
-      .valueChanges()
-      .pipe(takeUntil(this.ngUnsubscribe))
-      .subscribe((jogador: object) => {
-        for (let keyJogador in jogador) {
-          if (
-            jogador[keyJogador].timestamp + intervaloOffline <
-            firebase.database.ServerValue.TIMESTAMP
-          ) {
-            if (
-              !jogador[keyJogador].ativo &&
-              jogador[keyJogador].timestamp + intervaloKick <
-                firebase.database.ServerValue.TIMESTAMP
-            ) {
-              this.sairSala(keySala, keyJogador);
-            } else {
-              this.offline(keySala, keyJogador);
-            }
-          }
-        }
-      });
   }
 
   getSorteio() {}
@@ -101,26 +47,18 @@ export class TaskforceService {
   entrarSala(keySala: string, nick: string): string {
     var jogador = {
       nick: nick,
-      ativo: true,
-      timestamp: firebase.database.ServerValue.TIMESTAMP
+      ativo: true
     };
 
     return this.db.database.ref('salas/' + keySala + '/jogadores').push(jogador)
       .key;
   }
 
-  offline(keySala: string, keyJogador: string) {
-    this.db.database
-      .ref('salas/' + keySala + '/jogadores')
-      .child(keyJogador)
-      .update({ ativo: false });
-  }
-
   sairSala(keySala: string, keyJogador: string) {
-    this.db.database
-      .ref('salas/' + keySala + '/jogadores')
-      .child(keyJogador)
-      .remove();
+    this.db
+      .object('salas/' + keySala + '/jogadores/' + keyJogador)
+      .query.ref.onDisconnect()
+      .update({ativo: false});
   }
 
   adicionarRegistro(keySala: string, profissao: any, tarefa: any): string {
@@ -130,8 +68,7 @@ export class TaskforceService {
       id: tarefa.id,
       ativo: true,
       concluido: false,
-      texto: tarefa.verbo,
-      timestamp: firebase.database.ServerValue.TIMESTAMP
+      texto: tarefa.verbo
     };
 
     return this.db.database
@@ -146,7 +83,7 @@ export class TaskforceService {
     fConcluido: () => void,
     fPrescrito: () => void
   ) {
-    var tempoInicio = 0; //firebase.database.ServerValue.TIMESTAMP;
+    var tempoInicio = +new Date();
     var tempoFim;
 
     this.db
@@ -155,7 +92,7 @@ export class TaskforceService {
       .pipe(
         takeUntil(this.ngUnsubscribe),
         takeWhile(r => {
-          tempoFim = 0; //firebase.database.ServerValue.TIMESTAMP;
+          tempoFim = +new Date();
           return !(r['concluido'] || tempoInicio + tempoIntervalo > tempoFim);
         }, true)
       )
@@ -189,14 +126,5 @@ export class TaskforceService {
     this.db.database
       .ref('salas/' + keySala + '/registros/' + keyRegistro)
       .update({ ativo: false });
-  }
-
-  keepAlive(keySala: string, keyJogador: string) {
-    this.db.database
-      .ref('salas/' + keySala + '/jogadores/' + keyJogador)
-      .update({
-        timestamp: firebase.database.ServerValue.TIMESTAMP,
-        ativo: true
-      });
   }
 }
