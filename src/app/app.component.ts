83e3b4141559bc5e9bc681cccaa83242cd282069
fabioconejo <fabio.conejo@hotmail.com>
2022-02-06 18:39:30 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,26 +10,30 @@ import { Meta } from '@angular/platform-browser';
     trigger('fade', [
       transition(':enter', [
         style({ opacity: 0 }),
-        animate('300ms 300ms ease-out', style({ opacity: 1 }))
+        animate('300ms 300ms ease-out', style({ opacity: 1 })),
       ]),
       transition(':leave', [
         style({ opacity: 1 }),
-        animate('300ms ease-in', style({ opacity: 0 }))
-      ])
-    ])
-  ]
+        animate('300ms ease-in', style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent implements OnInit {
   constructor(private meta: Meta) {
     this.meta.addTag({ property: 'og:title', content: 'Task Force' });
     this.meta.addTag({
       property: 'og:description',
-      content: 'Junte a galera e jogue online agora mesmo'
+      content: 'Junte a galera e jogue online agora mesmo',
     });
     this.meta.addTag({
       property: 'og:image',
       content:
-        'https://raw.githubusercontent.com/fabioconejo/taskforce/master/src/assets/images/taskforce-print.png'
+        'https://raw.githubusercontent.com/fabioconejo/taskforce/master/src/assets/images/taskforce-print.png',
+    });
+    this.meta.addTag({
+      property: 'theme-color',
+      content: 'var(--bg-color)',
     });
   }
 
