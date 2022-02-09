4bae2d257ed1ed76fa7b744674f20dc9dbcdeafb
fabioconejo <fabio.conejo@hotmail.com>
2022-02-09 18:28:08 -0300

    2 files modified

--- src/app/monitor/monitor.component.ts
+++ src/app/monitor/monitor.component.ts
@@ -3,14 +3,22 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'monitor',
   templateUrl: './monitor.component.html',
-  styleUrls: ['./monitor.component.css']
+  styleUrls: ['./monitor.component.css'],
 })
 export class MonitorComponent implements OnInit {
   @Input() texto: string;
   @Input() tempo: number;
   @Input() pausa: boolean;
 
+  dica: boolean;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.dica = true;
+  }
+
+  fechar() {
+    this.dica = false;
+  }
 }
