ffd343a9432bb6cc2766910a8c55986e8608a66a
fabioconejo <fabio.conejo@hotmail.com>
2022-02-17 16:55:19 -0300

    1 file modified, 2 added

---
+++ src/app/contagem/contagem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'contagem',
+  templateUrl: './contagem.component.html',
+  styleUrls: ['./contagem.component.css']
+})
+export class ContagemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
