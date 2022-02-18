77f089270b7ae6b0e8b660b531f28765e6eb3dfe
fabioconejo <fabio.conejo@hotmail.com>
2022-02-17 21:11:57 -0300

    4 files modified

--- src/app/contagem/contagem.component.ts
+++ src/app/contagem/contagem.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { TaskforceService } from '../taskforce.service';
 
 @Component({
   selector: 'contagem',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contagem.component.css']
 })
 export class ContagemComponent implements OnInit {
+  @Input() keySala:string;
+  
+  numero:number = 3;
+  contagem:any;
 
-  constructor() { }
+  constructor(private taskForceService: TaskforceService) { }
 
   ngOnInit() {
+    this.contagem = setInterval(() => {
+      this.numero--;
+
+      if (this.numero <= 0) {
+        this.taskForceService.setStatusSala(this.keySala, 'jogo');
+        this.numero = 0;
+      }
+
+    }, 1600)
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.contagem);
   }
 
 }
