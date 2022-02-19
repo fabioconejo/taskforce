7f76c79ba612ea090da51032e6557bcaf032a31c
fabioconejo <fabio.conejo@hotmail.com>
2022-02-19 14:13:22 -0300

    1 file modified

--- src/app/relatorio/relatorio.component.ts
+++ src/app/relatorio/relatorio.component.ts
@@ -7,7 +7,7 @@ import { TaskforceService } from '../taskforce.service';
 @Component({
   selector: 'relatorio',
   templateUrl: './relatorio.component.html',
-  styleUrls: ['./relatorio.component.css']
+  styleUrls: ['./relatorio.component.css'],
 })
 export class RelatorioComponent implements OnInit {
   constructor(private taskForceService: TaskforceService) {}
@@ -29,26 +29,40 @@ export class RelatorioComponent implements OnInit {
 
   async ngOnInit() {
     let i: number = 0;
+    let j: number = 0;
     this.vida = this.vidas;
     this.desabilitado = true;
 
-    this.listaProfissoesSorteadas.pipe(take(1)).subscribe(async l => {
+    this.listaProfissoesSorteadas.pipe(take(1)).subscribe(async (l) => {
       for (i = 0; i < l.length; i++) {
         this.lista.push({
           imagem: l[i].imagem,
           responsavel: l[i].responsavel,
           profissao: l[i].profissao,
           acertos: l[i].acertos,
-          erros: l[i].erros
+          erros: l[i].erros,
         });
 
-        this.relatorioPartida.push({
-          imagem: l[i].imagem,
-          responsavel: l[i].responsavel,
-          profissao: l[i].profissao,
-          acertos: l[i].acertos,
-          erros: l[i].erros
-        });
+        for (j = 0; j < this.relatorioPartida.length; j++) {
+          if (this.relatorioPartida[j].responsavel === l[i].responsavel) {
+            this.relatorioPartida[j].imagem = l[i].imagem;
+            this.relatorioPartida[j].responsavel = l[i].responsavel;
+            this.relatorioPartida[j].profissao = l[i].profissao;
+            this.relatorioPartida[j].acertos += l[i].acertos;
+            this.relatorioPartida[j].erros += l[i].erro;
+            break;
+          }
+        }
+
+        if (j === this.relatorioPartida.length) {
+          this.relatorioPartida.push({
+            imagem: l[i].imagem,
+            responsavel: l[i].responsavel,
+            profissao: l[i].profissao,
+            acertos: l[i].acertos,
+            erros: l[i].erros,
+          });
+        }
 
         this.relatorioPartidaChange.emit(this.relatorioPartida);
 
@@ -62,6 +76,8 @@ export class RelatorioComponent implements OnInit {
     if (!this.desabilitado) {
       this.desabilitado = true;
       if (this.vida <= 0) {
+        this.relatorioPartida = [];
+        this.relatorioPartidaChange.emit(this.relatorioPartida);
         await this.taskForceService.resetarSala(this.keySala);
       }
       this.flagRelatorio = false;
